1038eca4be2794346f76ebd124d9fe1f03330932
munqizislookingfineasshell <167727884+munqizislookingfineasshell@users.noreply.github.com>
2024-06-09 23:47:53 +0800

    setup basic movement for the game

--- main1.py
+++ main1.py
@@ -5,51 +5,54 @@ win = pygame.display.set_mode((1100,700))
 pygame.display.set_caption("slumberRock")
 scr_width = 1100
 scr_height = 700
-x = 100
+x = 50
 y = 600
 width = 40
 height = 60
-vel =1/2
-steps = 0 
+vel =5
+gravity_vel = 5 
 gravity_direction = "down" 
 
+space_pressed = False
 
 run = True
 
 while run:
-    pygame.time.delay(100)
+    pygame.time.delay(50)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
     
-    if gravity_direction == "down":
-        y += vel
-    else:
-        y -= vel    
-    x += vel
-    steps += 1
-    if steps >= 10:
-        x = 100
-        y = 600
-        steps = 0
-
     keys = pygame.key.get_pressed()
     
-    if keys[pygame.K_UP] and y > vel and gravity_direction == "down":  
-        y -= vel
-    elif keys[pygame.K_DOWN] and y < scr_height - height - vel and gravity_direction == "down":
-        y += vel
-    elif keys[pygame.K_UP] and y < scr_height - height - vel and gravity_direction == "up":  
-        y += vel
-    elif keys[pygame.K_DOWN] and y > vel and gravity_direction == "up":
-        y -= vel
     if keys[pygame.K_SPACE]:
-        if gravity_direction == "down":
-            gravity_direction = "up"
-        else:
-            gravity_direction = "down"
-    
+        if not space_pressed:
+            if gravity_direction == "down":
+                gravity_direction = "up"
+            else:
+                gravity_direction = "down"
+            space_pressed = True
+    else:
+        space_pressed = False
+
+    x += vel
+
+    if gravity_direction == "down":
+        y += gravity_vel
+    else:
+        y -= gravity_vel    
+
+
+    if y > scr_height - height:
+        y = scr_height - height
+    elif y < 0:
+        y = 0
+
+    if x > scr_width - width:
+        x=0
+
+
     win.fill((0,0,0)) 
     pygame.draw.rect(win, (255,0,0), (x, y, width, height))   
     pygame.display.update() 
