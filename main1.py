784e0780347228dbfa70750ef861b340ed10ba54
munqizislookingfineasshell <167727884+munqizislookingfineasshell@users.noreply.github.com>
2024-06-09 19:31:06 +0800

    new code cuz its getting error

--- main1.py
+++ main1.py
@@ -1,36 +1,57 @@
 import pygame
 pygame.init()
 
-win = pygame.display.set_mode((500, 500))
-pygame.display.set_caption ("SlumberRock")
-
-x = 50
-y = 50
+win = pygame.display.set_mode((1100,700))
+pygame.display.set_caption("slumberRock")
+scr_width = 1100
+scr_height = 700
+x = 100
+y = 600
 width = 40
 height = 60
-vel = 5
+vel =1/2
+steps = 0 
+gravity_direction = "down" 
+
 
 run = True
+
 while run:
-  pygame.time.delay(100)
+    pygame.time.delay(100)
 
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:
-      run = False
-
-  keys = pygame.key.get_pressed()
-
-  if keys [pygame.K_LEFT]:
-    x -= vel
-  if keys [pygame.K_RIGHT]:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    
+    if gravity_direction == "down":
+        y += vel
+    else:
+        y -= vel    
     x += vel
-  if keys [pygame.K_UP]:
-    y -= vel
-  if keys [pygame.K_DOWN]:
-    y += vel
-  
-pygame.draw.rect(win, (255,0,0), (x, y, width, height))
-pygame.display.update()
+    steps += 1
+    if steps >= 10:
+        x = 100
+        y = 600
+        steps = 0
 
+    keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_UP] and y > vel and gravity_direction == "down":  
+        y -= vel
+    elif keys[pygame.K_DOWN] and y < scr_height - height - vel and gravity_direction == "down":
+        y += vel
+    elif keys[pygame.K_UP] and y < scr_height - height - vel and gravity_direction == "up":  
+        y += vel
+    elif keys[pygame.K_DOWN] and y > vel and gravity_direction == "up":
+        y -= vel
+    if keys[pygame.K_SPACE]:
+        if gravity_direction == "down":
+            gravity_direction = "up"
+        else:
+            gravity_direction = "down"
+    
+    win.fill((0,0,0)) 
+    pygame.draw.rect(win, (255,0,0), (x, y, width, height))   
+    pygame.display.update() 
+    
 pygame.quit()
-   
